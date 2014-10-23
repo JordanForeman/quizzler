32f6081910542b61655fb8e718abfa760dc89c40
Jordan Foreman <jordan.foreman@meredith.com>
2014-10-23 15:43:59 -0500

    Trying something

--- app/api/index.js
+++ app/api/index.js
@@ -5,7 +5,8 @@ var router = require('express').Router(),
 
 router.use('/', function(req, res, next){
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Methods", "GET");
+	res.header("Access-Control-Allow-Headers", "*");
 	next();
 });
 
